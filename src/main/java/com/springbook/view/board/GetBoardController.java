f486b4b6fd0d81b1cbb28523e0adfc5c6d2b559c
ddojai <ddojaigo@gmail.com>
2018-05-19 22:53:56 +0900

    * 스프링 퀵 스타트   * day3     * class 6-7 : Spring MVC 적용

--- src/main/java/com/springbook/view/board/GetBoardController.java
+++ src/main/java/com/springbook/view/board/GetBoardController.java
@@ -2,15 +2,15 @@ package com.springbook.view.board;
 
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
-import com.springbook.view.controller.Controller;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 public class GetBoardController implements Controller {
     @Override
-    public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
         System.out.println("글 상세 조회 처리");
 
         // 1. 검색할 게시글 번호 추출
@@ -24,8 +24,9 @@ public class GetBoardController implements Controller {
         BoardVO board = boardDAO.getBoard(vo);
 
         // 3. 검색 결과를 세션에 저장하고 목록 화면으로 이동한다.
-        HttpSession session = request.getSession();
-        session.setAttribute("board", board);
-        return "getBoard";
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("board", board);
+        mav.setViewName("getBoard");
+        return mav;
     }
 }
