59d6773ffb6728e6c31f6a99bb035c4d7b4471a7
김흥열 <favianred@gmail.com>
2018-07-26 21:18:45 +0900

    * spring boot   * init

---
+++ src/main/java/io/ddojai/github/springboot/SpringbootApplication.java
@@ -0,0 +1,12 @@
+package io.ddojai.github.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+}
