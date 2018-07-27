4e3c5a462721b3e6722b5a52926835b526a1873b
김흥열 <favianred@gmail.com>
2018-07-27 19:51:13 +0900

    * 스프링부트로 웹 서비스 출시하기   * JPA     * 게시물       PostsSaveRequestDto.java   * WebRestController.java     * 게시물 저장 추가   * application.yml     * h2 활성화

--- src/main/java/io/ddojai/webservice/web/WebRestController.java
+++ src/main/java/io/ddojai/webservice/web/WebRestController.java
@@ -1,13 +1,26 @@
 package io.ddojai.webservice.web;
 
+import io.ddojai.webservice.domain.posts.PostsRepository;
+import io.ddojai.webservice.domain.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, Spring Boot!";
+        return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+        postsRepository.save(dto.toEntity());
     }
 }
