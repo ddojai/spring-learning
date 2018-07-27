b3df6d8430f0f0e21b2f8d81d0f2a9d50e59e8a2
김흥열 <favianred@gmail.com>
2018-07-27 22:57:03 +0900

    * 스프링부트로 웹 서비스 출시하기   * build.gradle     * LocalDateTime 버그 대응을 위한 추가   * 생성/업데이트 시간 관련 자동 처리     * BaseTimeEntity.java       * 시간 자동 처리 클래스     * Posts.java       * BaseTimeEntity 상속     * Application.java       * EnableJpaAuditing 추가     * PostsRepositoryTest.java       * 시간 관련 테스트 추가

--- src/test/java/io/ddojai/webservice/domain/posts/PostsRepositoryTest.java
+++ src/test/java/io/ddojai/webservice/domain/posts/PostsRepositoryTest.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+import static junit.framework.TestCase.assertTrue;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -21,10 +23,6 @@ public class PostsRepositoryTest {
 
     @After
     public void cleanup() {
-        /*
-         이후 테스트 코드에 영향을 끼치지 않기 위해
-         테스트 메소드가 끝날때 마다 respository 전체 비우는 코드
-         */
         postsRepository.deleteAll();
     }
 
@@ -44,5 +42,23 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle(), is("테스트 게시글"));
         assertThat(posts.getContent(), is("테스트 본문"));
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        //given
+        LocalDateTime now = LocalDateTime.now();
+        postsRepository.save(Posts.builder()
+                .title("테스트 게시글")
+                .content("테스트 본문")
+                .author("jojoldu@gmail.com")
+                .build());
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertTrue(posts.getCreatedDate().isAfter(now));
+        assertTrue(posts.getModifiedDate().isAfter(now));
     }
 }
