7597e6980307e98f1fd785d6473b9e52291020d7
김흥열 <favianred@gmail.com>
2018-07-28 14:31:22 +0900

    * 스프링부트로 웹 서비스 출시하기   * build.gradle     * handlebars 추가   * 게시물     * PostsSaveRequestDto.java       * 빌더 추가     * PostsService.java       * 저장 및 트랜잭션 처리       * PostServiceTest.java         * 테스트   * 메인 페이지    * WebController.java      * 메인 페이지 처리      * WebControllerTest.java        * 테스트    * main.hbs      * handlebars      * view 처리      * lib        * bootstrap.min.css        * bootstrap.min.js        * jquery.min.js

--- src/main/java/io/ddojai/webservice/web/WebRestController.java
+++ src/main/java/io/ddojai/webservice/web/WebRestController.java
@@ -2,6 +2,7 @@ package io.ddojai.webservice.web;
 
 import io.ddojai.webservice.domain.posts.PostsRepository;
 import io.ddojai.webservice.domain.posts.PostsSaveRequestDto;
+import io.ddojai.webservice.service.PostsService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class WebRestController {
 
-    private PostsRepository postsRepository;
+    private PostsService postsService;
 
     @GetMapping("/hello")
     public String hello() {
@@ -20,7 +21,7 @@ public class WebRestController {
     }
 
     @PostMapping("/posts")
-    public void savePosts(@RequestBody PostsSaveRequestDto dto){
-        postsRepository.save(dto.toEntity());
+    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+        postsService.save(dto);
     }
 }
