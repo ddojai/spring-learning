38b3318a4269e5c3209fcedbb4f95befe9382f73
김흥열 <favianred@gmail.com>
2018-08-15 18:19:33 +0900

    * 스타트 스프링 부트   * ch06     * 게시물 수정/삭제

--- src/main/java/io/ddojai/controller/WebBoardController.java
+++ src/main/java/io/ddojai/controller/WebBoardController.java
@@ -39,12 +39,12 @@ public class WebBoardController {
     }
 
     @GetMapping("/register")
-    public void registerGET(@ModelAttribute("vo")WebBoard vo) {
+    public void registerGET(@ModelAttribute("vo") WebBoard vo) {
         log.info("register get");
     }
 
     @PostMapping("/register")
-    public String registerPOST(@ModelAttribute("vo")WebBoard vo, RedirectAttributes rttr) {
+    public String registerPOST(@ModelAttribute("vo") WebBoard vo, RedirectAttributes rttr) {
         log.info("register post");
         log.info("" + vo);
 
@@ -58,5 +58,49 @@ public class WebBoardController {
     public void view(Long bno, @ModelAttribute("pageVO") PageVO vo, Model model) {
         log.info("BNO: " + bno);
         repo.findById(bno).ifPresent(board -> model.addAttribute("vo", board));
+    }
+
+    @GetMapping("/modify")
+    public void modify(Long bno, @ModelAttribute("pageVO") PageVO vo, Model model) {
+        log.info("MODIFY BNO: " + bno);
+        repo.findById(bno).ifPresent(board -> model.addAttribute("vo", board));
+    }
+
+    @PostMapping("/delete")
+    public String delete(Long bno, PageVO vo, RedirectAttributes rttr) {
+        log.info("DELETE BNO: " + bno);
+
+        repo.deleteById(bno);
+
+        rttr.addFlashAttribute("msg", "success");
+
+        rttr.addAttribute("page", vo.getPage());
+        rttr.addAttribute("size", vo.getSize());
+        rttr.addAttribute("type", vo.getType());
+        rttr.addAttribute("keyword", vo.getKeyword());
+
+        return "redirect:/boards/list";
+    }
+
+    @PostMapping("/modify")
+    public String modifyPost(WebBoard board, PageVO vo, RedirectAttributes rttr) {
+        log.info("Modify WebBoard: " + board);
+
+        repo.findById(board.getBno()).ifPresent(origin -> {
+            origin.setTitle(board.getTitle());
+            origin.setContent(board.getContent());
+
+            repo.save(origin);
+            rttr.addFlashAttribute("msg", "success");
+            rttr.addAttribute("bno", origin.getBno());
+        });
+
+        // 페이징과 검색했던 결과로 이동하는 경우
+        rttr.addAttribute("page", vo.getPage());
+        rttr.addAttribute("size", vo.getSize());
+        rttr.addAttribute("type", vo.getType());
+        rttr.addAttribute("keyword", vo.getKeyword());
+
+        return "redirect:/boards/view";
     }
 }
