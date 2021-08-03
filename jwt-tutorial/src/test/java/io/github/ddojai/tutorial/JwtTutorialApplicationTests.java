935f31be4fc6aadf4f7da373718c4d5884188d2a
ddojai <favianred@gmail.com>
2021-08-03 23:02:39 +0900

    Init jwt-tutorial

---
+++ jwt-tutorial/src/test/java/io/github/ddojai/tutorial/JwtTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ddojai.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
