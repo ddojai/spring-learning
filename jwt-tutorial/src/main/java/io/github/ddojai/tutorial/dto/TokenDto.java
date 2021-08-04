193a18c39c6c9fcbc91f6702b67cae962e1d1b5b
ddojai <favianred@gmail.com>
2021-08-05 00:58:45 +0900

    Add dto, repository, login api (ch04)
    - https://github.com/SilverNine/spring-boot-jwt-tutorial

---
+++ jwt-tutorial/src/main/java/io/github/ddojai/tutorial/dto/TokenDto.java
@@ -0,0 +1,13 @@
+package io.github.ddojai.tutorial.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenDto {
+
+    private String token;
+}
---
+++ jwt-tutorial/src/main/java/io/github/ddojai/tutorial/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package io.github.ddojai.tutorial.repository;
+
+import io.github.ddojai.tutorial.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    @EntityGraph(attributePaths = "authorities")
+    Optional<User> findOneWithAuthoritiesByUsername(String username);
+}
